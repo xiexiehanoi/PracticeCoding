dcd9bb302cdc410ec2de3fb98fbb720c3d52022d
samkim <ksw593vn@gmail.com>
2024-04-11 11:38:45 +0900

    문자열 여러번 뒤집기, 배열 만들기5

---
+++ Level0/src/Day_240410/Practice2.java
@@ -0,0 +1,27 @@
+    package Day_240410;
+
+    import java.util.ArrayList;
+    import java.util.List;
+
+    public class Practice2 {
+        public List<Integer> solution(String[] intStrs, int k, int s, int l) {
+            List<Integer> answer = new ArrayList<>();
+            for(String strs: intStrs){
+                int subString = Integer.parseInt(strs.substring(s,s+l));
+                if(subString>k){
+                    answer.add(subString);
+                }
+            }
+            return answer;
+        }
+
+        public static void main(String[] args) {
+            String[] intStrs = {"0123456789","9876543210","9999999999999"};
+            int k =50000;
+            int s =5;
+            int l =5;
+            Practice2 practice2 = new Practice2();
+            List<Integer> result = practice2.solution(intStrs,k,s,l);
+            System.out.println("result: "+result);
+        }
+    }
