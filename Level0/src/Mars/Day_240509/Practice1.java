4ec90972910d791c112a31ab427a674597362fec
samkim <ksw593vn@gmail.com>
2024-05-11 22:55:22 +0900

    5/9 코딩테스트

---
+++ Level0/src/Mars/Day_240509/Practice1.java
@@ -0,0 +1,18 @@
+package Mars.Day_240509;
+
+public class Practice1 {
+    public static String solution(int age) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(age);
+        for(int i=0; i<sb.length(); i++){
+            sb[i]
+        }
+            return answer;
+    }
+
+    public static void main(String[] args) {
+        int age =23;
+        String result = solution(age);
+        System.out.println("result: "+result);
+    }
+}
